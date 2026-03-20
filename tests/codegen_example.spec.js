681b45e7b83c5a319cf2c294c1c0d0e72a04ce2b
Sriram Kukkadapu (QCD R) <sriramkukkadapu@gmail.com>
2026-03-20 15:07:06 +0530

    Sriram local changes

--- tests/codegen_example.spec.js
+++ tests/codegen_example.spec.js
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.amazon.in/', {waitUntil: 'domcontentloaded'});
-  await page.getByRole('searchbox', { name: 'Search Amazon.in' }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon.in' }).fill('iphone');
+  await page.goto('https://www.amazon.in/', { waitUntil: 'load' });
+  const searchBox = page.getByRole('searchbox', { name: 'Search Amazon.in' });
+  await searchBox.waitFor({ state: 'visible' });
+  await searchBox.click();
+  await searchBox.fill('iphone');
   await page.getByRole('button', { name: 'iphone 16 pro 256gb' }).click();
 });
